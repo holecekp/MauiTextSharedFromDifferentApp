6848849bf72fe899779f575271198daa9fd6c6bb
holecekp <holecekp@holecekp.com>
2024-08-25 14:56:12 +0200

    Update MAUI to the latest version. Fix route name.

--- MauiTextSharedFromDifferentApp/Platforms/Android/MainActivity.cs
+++ MauiTextSharedFromDifferentApp/Platforms/Android/MainActivity.cs
@@ -7,32 +7,32 @@ namespace MauiTextSharedFromDifferentApp
 {
     [Activity(Theme = "@style/Maui.SplashTheme",
         MainLauncher = true,
-        LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+        LaunchMode = LaunchMode.SingleTop,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     [IntentFilter(new[] { Android.Content.Intent.ActionSend }, Categories = new[] { Android.Content.Intent.CategoryDefault }, DataMimeType = "text/plain")]
     [Preserve(AllMembers = true)]
     public class MainActivity : MauiAppCompatActivity
     {
 
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override async void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // It is necessary to use different approach than in XF. In MAUI, the Activity is created after the shell. It is necessary
-            // to move to the translate page.
-            // Sharing still does not work. It crashes with System.InvalidOperationException: 'This window is already associated with an active Activity
-            // (MorseCode.MainActivity). Please override CreateWindow on MorseCode.App to add support for multiple
-            // activities https://aka.ms/maui-docs-create-windowor set the LaunchMode to SingleTop on MorseCode.MainActivity.'
+            // Sharing still does not work for LaunchMode.SingleTop. It crashes with System.InvalidOperationException:
+            // System.InvalidOperationException: 'This window is already associated with an active Activity (MauiTextSharedFromDifferentApp.MainActivity).
+            // Please override CreateWindow on MauiTextSharedFromDifferentApp.App to add support for multiple activities https://aka.ms/maui-docs-create-window
+            // or set the LaunchMode to SingleTop on MauiTextSharedFromDifferentApp.MainActivity.'
             if (Intent.Action == Android.Content.Intent.ActionSend)
             {
                 string text = Intent.GetStringExtra(Android.Content.Intent.ExtraText);
                 ShowTextFromAnotherAppPage.TextFromAnotherApp = text;
 
-                const string sharedTextPage = "//ShowTextFromAnotherAppPage";
-                Shell.Current.GoToAsync(new ShellNavigationState(sharedTextPage));
+                const string sharedTextPage = "//ShowTextFromAnotherAppPageRoute";
+                await Shell.Current.GoToAsync(new ShellNavigationState(sharedTextPage)).ConfigureAwait(false);
             }
         }
 
-        protected override void OnNewIntent(Android.Content.Intent intent)
+        protected override async void OnNewIntent(Android.Content.Intent intent)
         {
             base.OnNewIntent(intent);
 
@@ -41,8 +41,8 @@ namespace MauiTextSharedFromDifferentApp
                 string text = intent.GetStringExtra(Android.Content.Intent.ExtraText);
                 ShowTextFromAnotherAppPage.TextFromAnotherApp = text;
 
-                const string sharedTextPage = "//ShowTextFromAnotherAppPage";
-                Shell.Current.GoToAsync(new ShellNavigationState(sharedTextPage));
+                const string sharedTextPage = "//ShowTextFromAnotherAppPageRoute";
+                await Shell.Current.GoToAsync(new ShellNavigationState(sharedTextPage)).ConfigureAwait(false);
             }
         }
     }
